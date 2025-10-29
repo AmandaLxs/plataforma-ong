aa6f0c13d84e967d263e5feba7afbb4f1a2cc6fc
Amanda Lima <amandalima.181@icloud.com>
2025-10-29 16:01:55 -0300

    Update utils.js

--- entrega3-js/js/utils.js
+++ entrega3-js/js/utils.js
@@ -1,1 +1,33 @@
+// Máscara para CPF
+export function aplicarMascaraCPF(valor) {
+  return valor
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+}
+
+// Máscara para telefone
+export function aplicarMascaraTelefone(valor) {
+  return valor
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4,5})(\d{4})$/, '$1-$2');
+}
+
+// Máscara para CEP
+export function aplicarMascaraCEP(valor) {
+  return valor
+    .replace(/\D/g, '')
+    .replace(/(\d{5})(\d)/, '$1-$2');
+}
+
+// Exibir mensagens de sucesso/erro
+export function mostrarMensagem(texto, tipo = 'sucesso') {
+  const msg = document.getElementById('mensagem');
+  msg.textContent = texto;
+  msg.className = tipo === 'erro' ? 'alert erro' : 'alert sucesso';
+  msg.style.display = 'block';
+  setTimeout(() => (msg.style.display = 'none'), 3000);
+}
 
